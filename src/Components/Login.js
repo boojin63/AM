b60c370beb2041a0cc6f7665a410648bce287d31
boojin <boojin63@gmail.com>
2024-08-31 17:19:59 +0900

    Second Commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,57 @@
+import React, { useState } from 'react';
+import logoImage from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 import '../Css/Style.css'
+import axios from 'axios';
 
-const Login = () => {
-    return(
-        <div>로그인페이지</div>
-    );
-}
+const LogIn = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-export default Login;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // 서버로 로그인 요청
+      const response = await axios.post('/api/login', { userid: id, password });
+      const authToken = response.data.token;
+
+      navigate('/main');
+    } catch (error) {
+      alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="logo">
+        <img src={logoImage} alt="Around Me" />
+      </div>
+      <h1>Login</h1>
+      <p>Welcome to Around Me</p>
+      <form className='in' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      <p className="register-text">
+        아직 회원 가입을 못 하셨나요? <a href="/register">회원가입</a>
+      </p>
+    </div>
+  );
+};
+
+export default LogIn;
