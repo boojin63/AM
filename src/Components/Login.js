43532e89a1c45cfe0e422476ffe2cd920b2b117a
chaitae67 <chaitae67@gmail.com>
2024-09-01 18:29:00 +0900

    ???

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,6 +5,7 @@ import '../Css/Style.css';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+
 const LogIn = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
@@ -15,14 +16,14 @@ const LogIn = () => {
 
     try {
       // 서버로 로그인 요청
-      const response = await axios.post('/api/login', { userid: id, password });
+      const response = await axios.post('http://localhost:8080/login', { userid: id, password });
       const authToken = response.data.token;
 
       // 토큰을 쿠키에 저장
       Cookies.set('authToken', authToken, { expires: 1 / 24, secure: true, sameSite: 'Strict' });
 
       // 로그인 성공 후 메인 페이지로 이동
-      navigate('/main');
+      navigate('/');
     } catch (error) {
       alert('아이디 또는 비밀번호가 올바르지 않습니다.');
     }
